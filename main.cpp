c7c717c6b69fbca2009f7a89a018141fb09b35b9
jp05451 <jp05451@yahoo.com.tw>
2025-01-24 16:33:05 +0800

    修正 main.cpp 中的格式問題，調整空格和條件語句的排版

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include"ecdh.hpp"
+#include "ecdh.hpp"
 #include <iostream>
 
 using namespace std;
@@ -11,19 +11,22 @@ int main()
     alice.generate_key();
     bob.generate_key();
 
-    EC_POINT* alice_pub_key = alice.get_public_key();
-    EC_POINT* bob_pub_key = bob.get_public_key();
+    const EC_POINT *alice_pub_key = alice.get_public_key();
+    const EC_POINT *bob_pub_key = bob.get_public_key();
 
     alice.compute_key(bob_pub_key);
     bob.compute_key(alice_pub_key);
 
-    unsigned char* alice_shared_key = alice.get_shared_key();
-    unsigned char* bob_shared_key = bob.get_shared_key();
+    unsigned char *alice_shared_key = alice.get_shared_key();
+    unsigned char *bob_shared_key = bob.get_shared_key();
 
     // 比較共享金鑰是否相同
-    if (memcmp(alice_shared_key, bob_shared_key, alice.get_shared_key_len()) == 0) {
+    if (memcmp(alice_shared_key, bob_shared_key, alice.get_shared_key_len()) == 0)
+    {
         cout << "Shared key is the same." << endl;
-    } else {
+    }
+    else
+    {
         cout << "Shared key is different." << endl;
     }
 
