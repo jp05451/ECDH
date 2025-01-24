939bae082c345761b518e89816805a402e9acd17
jp05451 <jp05451@yahoo.com.tw>
2025-01-24 16:32:56 +0800

    更新 ECDH 類別以使用 EVP_PKEY，新增獲取公鑰字串和長度的方法

--- ecdh.hpp
+++ ecdh.hpp
@@ -2,6 +2,7 @@
 #include <openssl/evp.h>
 #include <openssl/err.h>
 #include <openssl/rand.h>
+#include <string>
 
 class ECDH
 {
@@ -11,13 +12,17 @@ public:
     void generate_key();
     void compute_key(const EC_POINT *peer_pub_key);
 
-    EC_POINT *get_public_key();
+    const EC_POINT *get_public_key();
+    unsigned char *get_public_key_str();
+    size_t get_public_key_len();
     unsigned char *get_shared_key();
     size_t get_shared_key_len();
     void print_key();
 
 private:
-    EC_KEY *key;
+    EVP_PKEY *key;
+    unsigned char *publicKey;
+    size_t publicKeyLen;
     unsigned char *sharedKey;
     size_t sharedKeyLen;
 };
